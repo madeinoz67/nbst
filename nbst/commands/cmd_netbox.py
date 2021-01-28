ca0c64f35dbcc6c3678892bb0494f35b9a52ebd2
Stephen Eaton <seaton@strobotics.com.au>
2021-01-28 16:24:38 +0800

    netbox list-vms successfully deserialising api results to netbox domain objects

--- nbst/commands/cmd_netbox.py
+++ nbst/commands/cmd_netbox.py
@@ -1,19 +1,29 @@
 import click
 from loguru import logger
+from marshmallow import EXCLUDE
 
 from nbst.cli import pass_info
+from nbst.domain.netbox_schema import VirtualMachineSchema
+from nbst.services.netbox import NetboxService
 
 
 class Context:
     def __init__(self):
-        self.netbox = None
+        self.netboxservice = None
 
 
 @click.group()
-@click.pass_context
-def cli(ctx):
+@click.option("--host", type=str, help="Netbox Host")
+@click.option("--apikey", type=str, help="Netbox API key")
+@click.option(
+    "--use-ssl", default=False, flag_value=True, show_default=True, help="Use SSL"
+)
+# @click.pass_context
+@pass_info
+def cli(ctx, host, apikey, use_ssl):
     """Netbox Utilities"""
     ctx.obj = Context()
+    ctx.obj.netboxservice = NetboxService(host, apikey, use_ssl)
 
 
 @cli.command()
@@ -33,20 +43,39 @@ def cli(ctx):
     flag_value=True,
     help="Removes orphaned NetBox objects",
 )
-@click.argument("netbox_hostname", type=str, metavar="<netbox>")
+@click.option(
+    "--prism",
+    default=False,
+    help="Prism Host name",
+)
 @click.argument("prism_hostname", type=str, metavar="<prism>")
-# @click.pass_context
 @pass_info
 def sync(ctx, netbox_hostname, prism_hostname, dryrun, prune=False):
     """Sync netbox with prism central server
 
-    This will update Netbox with the objects on the remote nutanix prism central server.
+    This will update Netbox with the objects on the remote Nutanix prism central server.
 
     Where:\n
-    <netbox> is the FQDN of the netbox server \n
     <prism> is the FQDN of the Nutanix Prism central server
     """
     click.echo("syncing....")
     logger.debug(
         f"Syncing Netbox host {netbox_hostname} from prism host {prism_hostname} prune={prune} dryrun={dryrun}"
     )
+
+
+@cli.command()
+@pass_info
+def list_vms(ctx):
+    """lists all virtual machines in netbox """
+    logger.debug("command: list_vms")
+    try:
+        vm_schema = VirtualMachineSchema(many=True, unknown=EXCLUDE)
+
+        vm_data = ctx.obj.netboxservice.get_vms()
+
+        results = vm_schema.load(vm_data)
+    except TypeError as e:
+        logger.exception(f"{e.args}")
+        exit(1)
+    logger.debug(results)
